f48c13487f938f11f786daf97ec3f33ce1c1b836
yugandhar <yugandhar.kamdi@gmail.com>
2015-04-06 00:04:26 +0530

    js modified

--- hello.js
+++ hello.js
@@ -1,1 +1,14 @@
-console.log("hello world !");
+
+var fs = require('fs');
+var extension = process.argv[3];
+
+ fs.readdir(process.argv[2], function(err, list){
+	 if (err) 
+		throw err;
+	 for(var i =0; i<list.length; i++)
+	 {
+		var lastIndexOfDot = list[i].lastIndexOf(".");
+		if(extension != list[i] && extension == list[i].substr(lastIndexOfDot+1))
+			console.log(list[i]);
+	 }
+ });
